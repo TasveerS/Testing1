13b216563585e7ca54604b8958b610bd31b78fd2
TasveerS <tasveersingh@hotmail.com>
2018-07-21 12:54:23 +0200

    Add project files.

---
+++ WorkWithNowey/Models/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WorkWithNowey.Models
+{
+    public class Class1
+    {
+        [Key]
+        public int TestID { get; set; }
+ 
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double FeeRate { get; set; }
+    }
+}
---
+++ WorkWithNowey/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkWithNowey.Startup))]
+namespace WorkWithNowey
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
